6b595d9ea65be1fd080bc391b2d4bf12f732c6dd
James <james.rudd45@gmail.com>
2019-12-09 16:33:24 +1100

    Updating Tracker.jsx, including name field

--- component/tracker.jsx
+++ component/tracker.jsx
@@ -3,7 +3,8 @@ import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 
 class Tracker extends Component {
     state = {
-        value: 40
+        value: 40,
+        name: "Player 1"
     }
 
     increaseValue = () => {
@@ -15,7 +16,8 @@ class Tracker extends Component {
     bigIncreaseValue = () => {
         this.setState({
             value: this.state.value+10
-        })
+        });
+        
     }
 
     decreaseValue = () => {
@@ -30,16 +32,27 @@ class Tracker extends Component {
         })
     }
 
+    changeName = () => {
+
+    }
+
     render() {
         return(
-            <View style={styles.row}>
-                <TouchableOpacity style={styles.background} onPress={this.decreaseValue} onLongPress={this.bigDecreaseValue}>
-                    <Text style={styles.textable}>-</Text>
-                </TouchableOpacity>
-                <Text style={styles.textable}>{this.state.value}</Text>
-                <TouchableOpacity style={styles.background} onPress={this.increaseValue} onLongPress={this.bigIncreaseValue}>
-                    <Text style={styles.textable}>+</Text>
-                </TouchableOpacity>
+            <View style={styles.column}>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.background} onPress={this.decreaseValue} onLongPress={this.bigDecreaseValue}>
+                        <Text style={styles.textable}>-</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.textable}>{this.state.value}</Text>
+                    <TouchableOpacity style={styles.background} onPress={this.increaseValue} onLongPress={this.bigIncreaseValue}>
+                        <Text style={styles.textable}>+</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity>
+                        <Text style={styles.nameText}>{this.state.name}</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
@@ -51,10 +64,19 @@ const styles = StyleSheet.create({
         width: "100%",
         justifyContent: "space-around"
     },
+    column: {
+        flexDirection: "column",
+        width: "100%",
+
+    },
     textable: {
         color: "yellow",
         fontSize: 50,
         paddingHorizontal: 10
+    },
+    nameText: {
+        color: "yellow",
+        fontSize: 15,
     },
     background: {
         // backgroundColor: "#fff"
